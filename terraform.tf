7d19d586b419754f9666241352436b361c0cd3ae
Francisco Rodriguez <FRodriguez@bluecedar.com>
2019-08-12 18:28:39 -0500

    Initial commit

---
+++ terraform.tf
@@ -0,0 +1,30 @@
+locals {
+  participants = [
+      "Gonzalo Lopez",
+      "Panchito",
+      "Oscar",
+      "Pedro",
+      "Monse",
+  ]
+  certified = [
+      "Gonzalo Lopez",
+      "Panchito",
+      "Oscar",
+  ]
+}
+
+resource "random_integer" "rand" {
+  min     = 0
+  max     = "${length(concat(local.participants,local.certified))}"
+}
+output "winner" {
+  value = "${element(concat(local.participants,local.certified),random_integer.rand.result)}"
+}
+
+output "all-Participants" {
+  value = "${concat(local.participants,local.certified)}"
+}
+
+output "max" {
+  value = "${length(concat(local.participants,local.certified))}"
+}
